2ba12712b1496c914a52ec9c4d7cc4a3a2e029b1
l4hsantos <lss52@discente.ifpe.edu.br>
2025-05-01 13:56:13 -0300

    Criar main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+def boas_vindas():
+    print("—" * 60)
+    print("     BEM-VINDO À LIVRARIA DAS CARTAS PERDIDAS".center(60))
+    print("—" * 60)
+    print(" Um lugar onde cada livro possui uma história esperando para ser reencontrada,\n A próxima grande aventura está a uma página de distância.")
+    print()
